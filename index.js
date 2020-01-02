c3a7fad23a39c8e1a5d6224ea3bc2333542f73ae
Brandon Lau <brandon.lau86@gmail.com>
2020-01-01 23:23:44 -0500

    Add prototype to Animal

---
+++ index.js
@@ -0,0 +1,14 @@
+function Animal(name,species) 
+{
+  this.name = name;
+  this.species = species;
+}
+
+Animal.prototype.speak = function() {
+  console.log(`${this.name} says hello!`)
+}
+
+
+// let tom = new Animal("tom", "tiger");
+// let bloop = new Animal("bloop", "panda");
+// let roost = new Animal("roost", "chicken");
